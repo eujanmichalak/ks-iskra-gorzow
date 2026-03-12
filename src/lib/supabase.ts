995bbb5feb5a6cb19b25851705670e4814084d3b
eujanmichalak <k7y8c9bmyk@privaterelay.appleid.com>
2026-03-12 17:14:54 +0100

    test

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,15 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xndpbhblywygqxgfidqx.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZHBiaGJseXd5Z3F4Z2ZpZHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2MDIxMzksImV4cCI6MjA4NDE3ODEzOX0.zwXyg7-8p6Z4CGyASDgIJaYgxjLm_Vj8sLaBq1OFSY4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-
-// Dodajemy zabezpieczenie, żeby build nie wywalał błędu
-if (!supabaseUrl || !supabaseAnonKey) {
-    console.warn("UWAGA: Brak kluczy Supabase w zmiennych środowiskowych!");
-}
-
-export const supabase = createClient(
-    supabaseUrl || 'https://placeholder.supabase.co',
-    supabaseAnonKey || 'placeholder'
-);
+// Jeśli brakuje kluczy, createClient wywali błąd, co jest lepsze niż działanie na "placeholderach"
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
